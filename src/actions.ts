007fe910a235f5220dc879c72add65de0509446d
JDR89 <joaquin.romero89@gmail.com>
2024-02-26 10:37:16 -0300

    tryadncatchs

--- src/actions.ts
+++ src/actions.ts
@@ -6,8 +6,9 @@ import { Prediction } from "../types";
 
 export const createPrediction = async ( formData: FormData): Promise<Prediction> => {
     
+   try {
     noStore()
-
+    
     const imageUrl = await fetch(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLAUDINARY_CLOUD_NAME}/image/upload?upload_preset=replicate-app&folder=replicate-app`,
       {
@@ -63,38 +64,16 @@ export const createPrediction = async ( formData: FormData): Promise<Prediction>
       credentials: "include",
     }).then((res) => res.json() as Promise<Prediction>);
 
-    // while(["starting","processing"].includes(prediction.status)){
-    //   prediction = await fetch("https://replicate.com/api/predictions/" + prediction.id, {
-    //     "headers": {
-    //       "accept": "*/*",
-    //       "accept-language": "es-ES,es;q=0.9,en;q=0.8",
-    //       "baggage": "sentry-public_key=3dc017e574684610bbc7fd3b5519a4e8,sentry-trace_id=02f28aa0701a46f2800bded1837d3e9f,sentry-sample_rate=0.1",
-    //       "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
-    //       "sec-ch-ua-mobile": "?0",
-    //       "sec-ch-ua-platform": "\"Windows\"",
-    //       "sec-fetch-dest": "empty",
-    //       "sec-fetch-mode": "cors",
-    //       "sec-fetch-site": "same-origin",
-    //       "sentry-trace": "02f28aa0701a46f2800bded1837d3e9f-99b38173cdf4a94d-0"
-    //     },
-    //     "referrer": "https://replicate.com/jagilley/controlnet-hough?prediction=tcui7ktbpj7f6spidbpifk4vlm",
-    //     "referrerPolicy": "same-origin",
-    //     "body": null,
-    //     "method": "GET",
-    //     "mode": "cors",
-    //     "credentials": "include"
-    //   }).then((res) => res.json() as Promise<Prediction>);
-
-     
-
-    //   await sleep(4000)
-    // }
-
+  
     return prediction
+   } catch (error) {
+    console.log(error)
+   }
   };
 
   export const getPrediction=async(id:string)=>{
-    noStore()
+    try {
+      noStore()
     
     return fetch("https://replicate.com/api/predictions/" + id, {
         "headers": {
@@ -116,4 +95,7 @@ export const createPrediction = async ( formData: FormData): Promise<Prediction>
         "mode": "cors",
         "credentials": "include"
       }).then((res) => res.json() as Promise<Prediction>);
+    } catch (error) {
+      console.log(error)
+    }
   }
